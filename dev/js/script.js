67ec72250d0cfa2dc09ba79636a05b6e2d64ce59
manu horta <manuelferreirahorta@gmail.com>
2022-01-24 15:47:53 +0100

    commit de site responsive

--- dev/js/script.js
+++ dev/js/script.js
@@ -9,6 +9,35 @@ hour.innerHTML = date_index.getHours();
 minute.innerHTML = date_index.getMinutes();
 second.innerHTML = date_index.getSeconds();
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function(){
+    functionStick();
+};
+// Get the navbar
+let myNav = document.querySelector(".main-nav");
+// Get the offset position of the navbar
+let sticky = 0;
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function functionStick(){
+    if(window.pageYOffset > sticky){
+        myNav.classList.add("sticky");
+    }else{
+        myNav.classList.remove("sticky");
+    }
+}
+// Burger Menu
+let myBurger = document.querySelector(".burger-menu");
+let navLink = document.querySelector(".nav-link");
+let link = document.querySelectorAll(".main-nav__link");
+let cross = document.querySelector('.cross');
 
+cross.addEventListener("click", hasClick);
 
-
+myBurger.addEventListener("click", hasClick);
+for(i=0; i<link.length; i++){
+    link[i].addEventListener("click", hasClick)
+}
+function hasClick(){
+    myBurger.classList.toggle("active");
+    navLink.classList.toggle("active");
+}
